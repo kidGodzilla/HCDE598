242f62087af80bec721b4ab685d199e035abdc75
Mike Sinkula <mike@mikesinkula.com>
2013-12-09 17:52:35 -0800

    More.

---
+++ multiple-pages-for-your-website/includes/footer.php
@@ -0,0 +1,11 @@
+</div>
+<!-- End Content -->
+
+<!-- Begin Footer -->
+<div id="footer">
+<p>&copy;2014 <a href="http://www.mikesinkula.com/">Mike Sinkula</a>. All rights reserved.</p>
+</div>
+<!-- End Footer -->
+
+</body>
+</html>
