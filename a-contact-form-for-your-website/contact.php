2ccaa9dac5a82c8c86d1547a191d3a8b15ae95e8
Mike Sinkula <mike@mikesinkula.com>
2013-12-11 15:29:30 -0800

    More

--- a-contact-form-for-your-website/contact.php
+++ a-contact-form-for-your-website/contact.php
@@ -3,19 +3,43 @@
     <!-- Begin Text -->
     <article id="text">
     <h2>Contact Me</h2>  
+    <p>Give me a shout!</p>
+    
+	<?php
+    
+    if ( isset ($_POST['submit']) ) { // if the submit button is clicked...
+    
+        $mailMesage = ("{$_POST['name']} {$_POST['email']} wrote: \n\n{$_POST['message']}"); // message of the email
+        
+        $mailSubject = "{$_POST['subject']}"; // subject of the email
+        
+        $mailRecipient = "sinkum@uw.edu"; // destination email address
+        
+        $mailSender = "From: {$_POST['email']}"; // the mail sender
+        
+        mail ($mailRecipient, $mailSubject , $mailMesage, $mailSender); // function to send the email
+        
+        print ("<p>Thank you for contacting me, <strong>{$_POST['name']}</strong>!</p>\n"); // notice that thanks the user
+    
+    }
+    
+    ?>
     
     <!-- Begin Contact Form -->
     <form action="contact.php" method="post" name="contact">
     <label for="name">Name:</label>
-    <input name="name" type="text">
+    <input name="name" type="text" required="required">
     <label for="email">Email:</label>
-    <input name="email" type="email">
+    <input name="email" type="email" required="required">
     <label for="subject">Subject:</label>
     <select name="subject">
       <option value="General Inquiry" selected="selected">General Inquiry</option>
       <option value="Specific Inquiry">Specific Inquiry</option>
       <option value="Stoopid Inquiry">Stoopid Inquiry</option>
     </select>
+    <label for="message">Message:</label>
+    <textarea name="message" cols="40" rows="5" required="required"></textarea>
+    <input name="submit" type="submit" value="Submit Message">
     </form>
     <!-- End Contact Form -->
     
