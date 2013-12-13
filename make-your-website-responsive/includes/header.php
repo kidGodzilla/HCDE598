d39ea7b466dd16182719a12829d39d3b8f2b2a34
Mike Sinkula <mike@mikesinkula.com>
2013-12-13 15:19:43 -0800

    More.

---
+++ make-your-website-responsive/includes/header.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Mike Sinkula</title>
+
+<!-- Begin Styles -->
+<link rel="stylesheet" href="styles.css" type="text/css" media="all" />
+<link rel="stylesheet" href="flexslider.css" type="text/css">
+<!-- End Styles -->
+
+<!-- Begin Scripts -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js"></script>
+<script src="scripts/jquery.flexslider.js"></script>
+<!-- End Scripts -->
+
+<!-- Begin Flex Slider -->
+<script type="text/javascript" charset="utf-8">
+  $(window).load(function() {
+    $('.flexslider').flexslider();
+  });
+</script>
+<!-- End Flex Slider -->
+
+
+</head>
+<body>
+
+<!-- Begin Header -->
+<div id="header">
+<h1><a href="index.php">Mike Sinkula</a></h1>
+</div>
+<!-- End Header -->
+
+<?php include('includes/navigation.php'); ?>
+
+<!-- Begin Content -->
+<div id="content">
---
+++ make-your-website-responsive/index.php
@@ -0,0 +1,37 @@
+<?php include('includes/header.php'); ?>
+
+  <!-- Begin Flex Slider -->
+    <div class="flexslider">
+        <ul class="slides">
+        <li><img src="images/img-slide-01.jpg" width="940" height="400" alt="Image One"></li>
+        <li><img src="images/img-slide-02.jpg" width="940" height="400" alt="Image Two"></li>
+        <li><img src="images/img-slide-03.jpg" width="940" height="400" alt="Image Three"></li>
+        </ul>
+    </div>
+    <!-- End Flex Slider -->
+    
+    <!-- Begin Widgets -->
+    <div id="widgets">
+        <section class="widget-item">
+            <h2>About Me</h2>
+            <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet.</p>
+        </section>
+        <section class="widget-item">
+            <h2>Services</h2>
+            <p>Lorem ipsum dolor sit amet:</p>
+            <ul>
+            <li>Lorem ipsum dolor</li>
+            <li>Aliquam tincidunt</li>
+            <li>Vestibulum auctor</li>
+            <li>Pellentesque habitant</li>
+            </ul>
+        </section>
+        <section class="widget-item">
+            <h2>Contact Me</h2>
+            <p><strong>Phone: </strong>206.543.2567<br><strong>Email: </strong><a href="mailto:sinkum@uw.edu">sinkum@uw.edu</a></p>
+            <p>428 Sieg Hall<br>University of Washington<br>Seattle, WA 98195</p> 
+        </section>
+    </div>
+    <!-- End Widgets -->
+    
+<?php include('includes/footer.php'); ?>
